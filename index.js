ebb6943bc9ef38bc6a55ea76f41fcc41ac76e0fb
NicolasBenitez30 <nicolasbenitez1438@gmail.com>
2022-12-02 16:05:48 -0300

    o

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { render } = require('ejs')
 const { req, res } = require('express')
 const express = require('express')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 
@@ -11,9 +12,9 @@ const port = process.env.PORT
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.use(express.static('public'))
 
 app.set('views', './views')
-
 app.set('view engine', 'ejs')
 
 
